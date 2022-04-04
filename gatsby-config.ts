2411d1d644330f86666d613ec7cc2ff5207c0af7
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-04-04 10:09:55 +0200

    Add tailwind and prettier as dependencies to the project

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,17 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: ``,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-react-helmet"]
+	siteMetadata: {
+		title: 'LA regional Bau',
+		description:
+			'Benötigen Sie Unterstützung in Stadt- und Landkreis Landshut bei der Verwirklichung Ihrer Projekte und Wünsche Rund ums Haus? Dann bin ich ihr Fachmann!',
+		siteUrl: `https://www.la-regionalbau.de`,
+		social: {
+			instagram: '',
+			facebook: '',
+		},
+	},
+	plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-postcss'],
 };
 
 export default config;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#E31E24',
+				secondary: '#727271',
+				white: '#FFF',
+				black: '#000',
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
